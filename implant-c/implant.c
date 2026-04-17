8ce32aec2dac6ed6d1e5cdc067a36820537d6acc
a-rvid <git@rvid.eu>
2026-04-17 16:53:20 +0200

    make

--- implant-c/implant.c
+++ implant-c/implant.c
@@ -1,20 +1,6 @@
-long write(long fd, const void *buf, long count) {
-    long ret;
-    asm volatile(
-        "syscall"
-        : "=a"(ret)
-        : "a"(1), "D"(fd), "S"(buf), "d"(count)
-        : "rcx", "r11"
-    );
-    return ret;
-}
+#include <unistd.h>
 
-long exit(long code) {
-    asm("syscall" : : "a"(60), "D"(code));
-    return 0;
-}
-
-void _start() {
+int main() {
     write(1, "hllo world", 10);
-    exit(0);
+    return 0;
 }
